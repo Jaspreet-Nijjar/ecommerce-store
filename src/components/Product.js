d324439f0ee43a6fdb543a4e3dd62358ae24f9ef
Jaspreet <jaspreetkn92@gmail.com>
2023-05-03 18:18:52 +0100

    added products and styling

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,3 +1,14 @@
+import './Product.css';
+
 export const Product = ({ product }) => {
-  return <p>{product.price}</p>;
+  return (
+    <div className="product-list">
+      <div className="product-card">
+        <img src={product.image} alt="products of accessories and clothing" />
+        <p className="product-title">{product.title}</p>
+        <p className="product-price">${product.price.toFixed(2)}</p>
+        <button className="product-btn">Add to cart</button>
+      </div>
+    </div>
+  );
 };

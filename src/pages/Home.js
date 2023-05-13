464d0c8af234eb0d8493bf4c39cdb47441a5e5a8
Jaspreet <jaspreetkn92@gmail.com>
2023-05-13 21:31:34 +0100

    changed home page design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,42 +2,25 @@ import './Home.css';
 import HomeHero from '../assets/home-image.jpg';
 import Electronics from '../assets/electronics.jpg';
 import Jewellery from '../assets/jewellery.jpg';
-import { useFetch } from '../hooks/useFetch';
 import { Link } from 'react-router-dom';
 
 export const Home = () => {
-  const { products } = useFetch('https://fakestoreapi.com/products?limit=6');
-
   return (
-    <section className="home-container">
-      <div className="home-hero">
+    <section className="home-container wrapper">
+      <div className="home-content wrapper">
+        <h1>Explore the best online store!</h1>
         <img
           src={HomeHero}
-          alt="woman scrolling on the phone on the couch"
-          className="home-hero-image"
+          alt="woman sitting on the phone on the couch"
+          className="home-hero-img"
         />
-
-        <img
-          src={Jewellery}
-          alt="shiny silver bracelet"
-          className="home-hero-image"
-        />
-      </div>
-      <div className="home-content">
-        <h1>Explore the best online store!</h1>
         <p>
-          Find everything you need! Between clothing, jewellery and electronics
-          - we have it all!
+          Find everything you need! Between{' '}
+          <strong>clothing, jewellery and electronics</strong> - we have it all!
         </p>
-        <div className="home-products-container"></div>
         <Link to="/products" className="home-btn">
           Shop Now!
         </Link>
-        <img
-          src={Electronics}
-          alt="electronic devices including headphones and laptop"
-          className="home-electronics-img"
-        />
       </div>
     </section>
   );

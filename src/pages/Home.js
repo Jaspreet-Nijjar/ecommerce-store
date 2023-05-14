ec08dbab1dc8d6fd4bebade2b5854fb7bbe84be8
Jaspreet <jaspreetkn92@gmail.com>
2023-05-14 13:50:59 +0100

    added featured products to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import './Home.css';
 import HomeHero from '../assets/home-image.jpg';
 import { Link } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
 
 export const Home = () => {
+  const { products } = useFetch('https://fakestoreapi.com/products?limit=4');
+
   return (
     <section className="home-container wrapper">
       <h1 className="home-header">Explore the best online store!</h1>
@@ -20,10 +23,29 @@ export const Home = () => {
           alt="woman sitting on the phone on the couch"
           className="home-hero-img"
         />
+      </div>
 
+      <div className="home-link">
         <Link to="/products" className="home-btn">
           Shop Now!
         </Link>
+      </div>
+
+      <div className="home-products">
+        <h3>Featured Products</h3>
+
+        <div className="product-container">
+          {products.map((product) => (
+            <div className="product-content">
+              <img
+                src={product.image}
+                alt="an image of a product"
+                className="home-product-img"
+              />
+              <p className="product-title">{product.title}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );

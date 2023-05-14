de51573861d81d87943737b4f4046fe5be2dbd7c
Jaspreet <jaspreetkn92@gmail.com>
2023-05-14 13:06:03 +0100

    redesigning the home page and added new font styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,26 @@
 import './Home.css';
 import HomeHero from '../assets/home-image.jpg';
-import Electronics from '../assets/electronics.jpg';
-import Jewellery from '../assets/jewellery.jpg';
 import { Link } from 'react-router-dom';
 
 export const Home = () => {
   return (
     <section className="home-container wrapper">
+      <h1 className="home-header">Explore the best online store!</h1>
       <div className="home-content wrapper">
-        <h1>Explore the best online store!</h1>
+        <div className="home-copy">
+          <h2 className="home-subtitle">Find everything you need!</h2>
+          <p>
+            We have the latest fashion trends with the new clothes, jewellery
+            and technologies
+          </p>
+        </div>
+
         <img
           src={HomeHero}
           alt="woman sitting on the phone on the couch"
           className="home-hero-img"
         />
-        <p>
-          Find everything you need! Between{' '}
-          <strong>clothing, jewellery and electronics</strong> - we have it all!
-        </p>
+
         <Link to="/products" className="home-btn">
           Shop Now!
         </Link>
